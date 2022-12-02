fd2a60184ac50b2b167aa4c665fb75aa338bbb44
glassycake <110797116+glassycake@users.noreply.github.com>
2022-12-02 09:53:06 -0800

    add book blurbs

--- data.js
+++ data.js
@@ -3,27 +3,27 @@ export const books = [
     title: "I Miss You, I Hate This",
     author: "Sara Saedi",
     img: "book-one.jpg",
-    blurb: "This book is about...",
+    blurb: "Five Feet Apart meets Kate in Waiting in this timely story of two best friends navigating the complexities of friendship while their world is turned upside down by a global pandemic, from the author of Americanized: Rebel Without a Green Card.",
     url: "rKmeW_bDfGI"
 },
 {
     title: "The Getaway",
     author: "Lamar Giles",
     img: "book-two.jpg",
-    blurb: "This book is about...",
+    blurb: "Jay is living his best life at Karloff Country, one of the world’s most famous resorts. He’s got his family, his crew, and an incredible after-school job at the property’s main theme park. Life isn’t so great for the rest of the world, but when people come here to vacation, it’s to get away from all that.",
     url: "pplnjhv5uMw"
 },
 {
     title: "The Life and Crimes of Hoodie Rosen",
     author: "Isaac Blum",
     img: "book-three.jpg",
-    blurb: "This book is about...",
+    blurb: "Hoodie Rosen's life isn't that bad. Sure, his entire Orthodox Jewish community has just picked up and moved to the quiet, mostly non-Jewish town of Tregaron, but Hoodie's world hasn't changed that much. He's got basketball to play, studies to avoid, and a supermarket full of delicious kosher snacks to eat. The people of Tregaron aren’t happy that so many Orthodox Jews are moving in at once, but that’s not Hoodie’s problem.",
     url: "FjLSZTQxZ_E"
 },
 {
     title: "Self-Made Boys",
     author: "Anna-Marie McLemore",
     img: "book-four.jpg",
-    blurb: "This book is about...",
+    blurb: "Nick’s neighbor in West Egg is a mysterious young man named Jay Gatsby, whose castle-like mansion is the stage for parties so extravagant that they both dazzle and terrify Nick. At one of these parties, Nick learns that the spectacle is all for the benefit of impressing a girl from Jay’s past—Daisy. And he learns something else: Jay is also transgender.",
     url: "pneMaMhtut4"
 }]
