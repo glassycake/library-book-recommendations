5cdaa88850f944bc9d2a1f7a3aca47174f55816e
glassycake <110797116+glassycake@users.noreply.github.com>
2022-12-05 11:44:13 -0800

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const books = [
 {
     title: "Nyxia",
     author: "Scott Reintgen",
-    img: "book-one.jpg",
+    img: "book-one.jfif",
     blurb: "Emmett Atwater is leaving Earth. Why the Babel Corporation recruited him is a mystery, but the number of zeroes on their contract has him boarding their lightship and hoping to return to Earth with enough money to take care of his family. Before long, Emmett discovers that he is one of ten recruits, all of whom have troubled pasts and are a long way from home. But Babel’s ship is full of secrets. And Emmett will face the ultimate choice: win the fortune at any cost, or find a way to fight that won’t forever compromise what it means to be human.",
     url: "E21NKO8Oe6U"
 },
